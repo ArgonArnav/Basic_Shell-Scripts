be73c843e6f73e556b979eecefc7ffcbedc6f810
Arnav Anand <anandarnav2016@gmail.com>
2019-11-26 03:11:28 +0530

    Create Basic_Commands.sh

---
+++ Basic_Commands.sh
@@ -0,0 +1,49 @@
+# Echo 
+# Used to display line of text/string that are passed as an argument.
+
+# Cat 
+# It reads data from the file and gives their content as output. It helps us to create, view, concatenate files.
+
+# whoami 
+# Displays the username of the current username when this command is invoked.
+
+# Who am i 
+# Gives complete information
+
+# who 
+# Lets you display the users that are currently logged into your system.
+
+# Clear
+# Clears the terminal screen.
+
+# Calendar
+# Invoke the cal command to see the calendar logged into your system.
+
+# Date 
+# Displays the date & time to the nearest second.
+
+# Uptime 
+# Returns info about how long your system has been running together with the current time, number of words, number 
+# of users with running session & the system load leverages for past 1,5&15 minutes.
+
+# Uname
+# Reports basic info about computer software and hardware.
+
+# Logname 
+# Reports the name of the user currently logged in.
+
+# ulimit
+# Allows you to display & set resource limits for users.
+
+# dir
+# Is used to display and set resource limits for users.
+
+# man
+# Used to display user manual of any command we can run on terminal.
+
+# Hostname
+# Used to obtain DNS (domain name system) and set the system’s hostname or NTS(Network Information System) domain name.
+
+# tty
+# Displays information related to terminal, basically prints the file name of the terminal connected to standard input.It also return terminal number.
+
