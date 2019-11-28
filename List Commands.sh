d83f0c05a8ccc5ba09c1c1a278f2a4a8a081efa9
Arnav Anand <anandarnav2016@gmail.com>
2019-11-28 12:21:09 +0530

    Create List Commands.sh

---
+++ List Commands.sh
@@ -0,0 +1,16 @@
+  ls
+# With no option lists are files and directories.
+
+  ls -l
+# to show long listing information about the file or directory.
+
+  Ls -a
+# shows all files regardless of hidden or public.
+
+  Ls -r 
+# List all files in reverse order.
+
+Ls -1
+# Displays one file per line.
+
+
