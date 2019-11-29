409a442acd8bcabdcf8b7a53e25bec61aa61fde5
Arnav Anand <anandarnav2016@gmail.com>
2019-11-29 16:43:48 +0530

    Create Wild Characters & Word Count.sh

---
+++ Wild Characters & Word Count.sh
@@ -0,0 +1,17 @@
+?   
+# matches a single character.
+*   
+# matches any number of characters.
+-
+# gives a range of characters. 
+,
+# matches particular characters.
+
+wc filename
+# Used to find out number of newline count, word count, byte & characters count in the files specified by the file arguments.
+wc -l 
+# Prints number of lines in a file.
+wc -w
+# Prints number of words in a file.
+wc -c
+# Prints counts of bytes in a file.
