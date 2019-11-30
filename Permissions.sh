0d95e12d154dc47ce1b6045c88ad5bfd761094c6
Arnav Anand <anandarnav2016@gmail.com>
2019-11-30 18:58:54 +0530

    Create Permissions.sh

---
+++ Permissions.sh
@@ -0,0 +1,31 @@
+  chmod (change mode)
+# Used for changing the access mode of the file 
+
+  chgrp 
+# Used to change the group ownership of a file or directory.
+
+  chown
+# Used to change file owner or group.
+
+REFERENCE
+  u
+# File owner
+  
+  g
+# Users who are members of all the file group
+  
+  o
+# Users who are neither the files owners nor members of files
+  
+  a
+# All of the above
+
+OPERATORS
+  +
+# Adds the specified modes to the specified classes
+
+  -
+# Removes the specified modes from the specified classes
+
+  =
+# The modes specified are to be made the exact modes for the specified classes
