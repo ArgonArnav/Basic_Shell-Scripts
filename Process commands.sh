9e984ee3422af7a54bd4697018660d40c55f1f50
Arnav Anand <anandarnav2016@gmail.com>
2019-12-02 22:51:55 +0530

    Create Process commands.sh

---
+++ Process commands.sh
@@ -0,0 +1,23 @@
+  Ps 
+# Lists running on your system.
+
+  kill
+# Kill a process, given its process ID.
+
+  fg
+# Used to continue a program which was stopped & bring it to the foreground.
+
+  top
+# Tells users about all the running processes in the system.
+
+  nice
+# Prioritises processes as per user requirements. It has a value between -20 & 19.
+
+  renice
+# Changes the nice value of an already running process.
+
+  bg
+# Restart a stopped a background process.
+
+  Ps -f
+# Full list of processes_priority, uptime, usage details, etc.
